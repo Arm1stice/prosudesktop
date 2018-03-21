5cd0507788e57c269e7f1e99f5c2454e4fc1bc4d
Wyatt Calandro <arm1stice@arm1stice.com>
2018-03-20 21:45:35 -0700

    Remove un-needed return statements

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import { app, BrowserWindow } from 'electron';
 var isDev = require("electron-is-dev")
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  return app.quit();
+  app.quit();
 }
 
 const isSecondInstance = app.makeSingleInstance((commandLine, workingDirectory) => {
   // Open dialog
 });
 if (isSecondInstance) {
-  return app.quit()
+  app.quit()
 }
 
 app.on('ready', () =>{
